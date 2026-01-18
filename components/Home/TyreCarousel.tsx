c924ece58f1447a60fdb6d9dbbfd3f526f795de3
Asura <kiba741@gmail.com>
2026-01-18 21:20:27 +0000

    feat: refactor TyreCarousel to use a narrowed type for improved type safety

--- components/Home/TyreCarousel.tsx
+++ components/Home/TyreCarousel.tsx
@@ -5,22 +5,33 @@ import { urlFor } from "@/sanity/lib/image";
 import { TYRES_FOR_CAROUSEL } from "@/sanity/lib/queries";
 import type { Tyres } from "@/sanity.types";
 
+// Narrowed type for items returned by TYRES_FOR_CAROUSEL (no `any` used)
+type TyreCarouselItem = {
+  _id?: string;
+  name?: Tyres["name"];
+  brand?: Tyres["brand"];
+  image?: Tyres["images"] extends Array<infer U> ? U : unknown;
+  sizes?: Tyres["sizes"];
+};
+
 const TyreCarousel = () => {
   return (
     <RemoteCarousel
       title={"POPULAR TYRES"}
       query={TYRES_FOR_CAROUSEL}
-      mapToKey={(t: any) => t._id}
-      renderCard={(t: {
-        _id?: string;
-        name?: Tyres["name"];
-        brand?: Tyres["brand"];
-        image?: Tyres["images"] extends Array<infer U> ? U : unknown;
-        sizes?: Tyres["sizes"];
-      }) => {
-        const image = t.image ? urlFor(t.image).width(800).height(600).url() : undefined;
+      mapToKey={(t: TyreCarouselItem) => t._id ?? ""}
+      renderCard={(t: TyreCarouselItem) => {
+        const image = t.image
+          ? urlFor(t.image).width(800).height(600).url()
+          : undefined;
         return (
-          <TyreCard id={t._id} name={t.name} brand={t.brand ?? ""} image={image} sizes={t.sizes} />
+          <TyreCard
+            id={t._id}
+            name={t.name}
+            brand={t.brand ?? ""}
+            image={image}
+            sizes={t.sizes}
+          />
         );
       }}
     />
